65196dcf29b6b4722adc3bd8921e94cb8aa570b7
Błażej Kotowski <blaziko@gmail.com>
2014-02-11 15:06:56 +0100

    Service registration and factory

---
+++ src/rest/service.cpp
@@ -0,0 +1,20 @@
+#include "service.h"
+
+namespace REST {
+
+  services_map* ServiceFactory::services = NULL;
+
+  Service* ServiceFactory::createInstance(string const &name) {
+    services_map::iterator iter= Map()->find(name);
+    if(iter == Map()->end())
+      return NULL;
+    return iter->second();
+  }
+
+  services_map* ServiceFactory::Map() {
+    if(services == NULL)
+      services = new services_map;
+    return services;
+  }
+
+}
--- src/rest/service.h
+++ src/rest/service.h
@@ -1,6 +1,10 @@
 #ifndef REST_CPP_SERVICE_H
 #define REST_CPP_SERVICE_H
 
+#include <iostream>
+#include <map>
+using namespace std;
+
 namespace REST {
 
 /**
@@ -11,6 +15,28 @@ namespace REST {
 class Service {
 
 };
+
+
+template <typename T>
+Service * createService() {
+  return new T;
+}
+
+typedef map<string, Service*(*)()> services_map;
+class ServiceFactory {
+  public:
+    static Service * createInstance(string const&);
+    static services_map* Map();
+  private:
+    static services_map* services;
+};
+
+template <typename C>
+struct ServiceRegister : ServiceFactory {
+  ServiceRegister(string const& name) {
+    Map()->insert(make_pair(name, &createService<C>);
+  }
+}
 
 }
 
