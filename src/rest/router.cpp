24a1c5343f1dac5265a7cd873358f9486773b1c9
Błażej Kotowski <blaziko@gmail.com>
2014-02-11 19:19:08 +0100

    separate service for each worker

--- src/rest/router.cpp
+++ src/rest/router.cpp
@@ -6,6 +6,7 @@
 
 namespace REST {
 
+  workers_services_map* Router::workers_services = new workers_services_map();
   Router* Router::pInstance = NULL;
 
   Router* Router::Instance() {
@@ -14,7 +15,7 @@ namespace REST {
     return pInstance;
   }
 
-  Service* Router::getResource(std::string const& path) {
+  Service* Router::getResource(std::string const& path, int worker_id) {
     if (path.size() == 0)
       return NULL;
 
@@ -23,7 +24,26 @@ namespace REST {
     if(name.find("/") != std::string::npos)
       name = name.substr(name.find("/"));
     std::cout << "name: " << name << std::endl;
-    return ServiceFactory::createInstance(name);
+
+    workers_services_map::iterator witer = workers_services->find(worker_id);
+    names_services_map* names_services;
+    if(witer == workers_services->end()) {
+      names_services = new names_services_map();
+      workers_services->insert(std::make_pair(worker_id,names_services));
+    } else {
+      names_services = witer->second;
+    }
+
+    Service* service;
+    names_services_map::iterator niter = names_services->find(name);
+    if(niter == names_services->end()) {
+      service = ServiceFactory::createInstance(name);
+      names_services->insert(std::make_pair(name, service));
+    } else {
+      service = niter->second;
+    }
+
+    return service;
   }
 
 }
--- src/rest/router.h
+++ src/rest/router.h
@@ -2,9 +2,12 @@
 #define REST_CPP_ROUTER_H
 
 #include <iostream>
+#include <map>
 #include "service.h"
 
 namespace REST {
+typedef std::map<std::string, Service *> names_services_map;
+typedef std::map<int, names_services_map *> workers_services_map;
 
 /**
  * Router resolves URL to Request::parameters and
@@ -16,11 +19,12 @@ namespace REST {
 class Router {
   public:
     static Router* Instance();
-    static Service* getResource(std::string const &);
+    static Service* getResource(std::string const &, int);
   private:
     Router() {};
     Router(Router const&) {};
     static Router* pInstance;
+    static workers_services_map* workers_services;
 
 };
 
