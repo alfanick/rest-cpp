c57077a15ae43f6e9ace860ed4cc987ee828bc9e
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2016-01-15 11:27:30 +0100

    basics

--- example/todo_server/init.cpp
+++ example/todo_server/init.cpp
@@ -7,16 +7,8 @@ void routes(REST::Router* r) {
   r->resource<List>("/:list_id");
   r->resources<Task>("/:list_id/task");
   r->match("/foo", [](REST::LambdaService* s) {
-    // s->ensure_session();
-
-    // if (s->session->data["counter"].empty())
-    //   s->session->data["counter"] = 0;
-    //
-    // s->session->data["counter"] = s->session->data["counter"].asLargestUInt() + 1;
-
-    // s->response->raw = "Hello, world!";
     s->response->use_json();
-    s->response->data["counter"] = 0;
+    s->response->data["foo"] = 0;
   });
 }
 
